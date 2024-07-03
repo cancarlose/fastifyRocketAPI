543934c76d881adb3d293ca433628dc763728177
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-03 20:11:16 -0300

    migration file base

---
+++ db/migration_stub.ts
@@ -0,0 +1,7 @@
+export async function up(): Promise<void> {
+  // Escreva o código para a migração 'up' aqui
+}
+
+export async function down(): Promise<void> {
+  // Escreva o código para reverter a migração 'down' aqui
+}
