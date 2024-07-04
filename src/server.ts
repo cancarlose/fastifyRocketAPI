4fd44d7116543471762fcc1188f0adaec794cef7
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-03 22:11:44 -0300

    create a random route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -8,14 +10,20 @@ const app = fastify()
 // http://localhost:3333/hello
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server runing!')
