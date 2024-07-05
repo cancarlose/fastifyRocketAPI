0beee7d4821f43319938424aa747a2e4c5a0cacc
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-05 15:59:54 -0300

    split the get route to a route file

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
-
-// http://localhost:3333/hello
-
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
