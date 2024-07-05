673524318b015b7915b04ad219535415089d619c
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-05 16:00:37 -0300

    create the transaction route file

---
+++ src/routes/transactions.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    // { title, amount, type: crédito ou débito }
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transations').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // HTTP Codes: 201 - criado
+
+    reply.status(201).send()
+  })
+}
