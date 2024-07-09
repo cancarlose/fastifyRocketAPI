db40d8118752c83cbada320c069b723b5cd5b972
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-09 15:34:52 -0300

    creation of listing routes, unique listing with Id, and total account with the implementation of user cookie creation plus verification by the middleware

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,37 +2,54 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return { transactions }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const { sessionId } = request.cookies
+
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     return { transaction }
   })
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    return { summary }
-  })
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+
+      return { summary }
+    },
+  )
 
   app.post('/', async (request, reply) => {
-    // { title, amount, type: crédito ou débito }
-
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
@@ -60,8 +77,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
       amount: type === 'credit' ? amount : amount * -1,
       session_id: sessionId,
     })
-
-    // HTTP Codes: 201 - criado
 
     reply.status(201).send()
   })
