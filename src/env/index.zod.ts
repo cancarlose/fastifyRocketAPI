73169ed26c4663b2b4134afbcec17aa46a410c1b
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-09 22:16:54 -0300

    implementation of zod to check the type of environment variable being worked on

--- src/env/index.zod.ts
+++ src/env/index.zod.ts
@@ -1,7 +1,13 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
 // process.env
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
