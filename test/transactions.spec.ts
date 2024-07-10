8f9d8c5b59b61e087b67a433f11b241dc9be18bb
Carlos Eduardo <carlosedlima4@gmail.com>
2024-07-09 21:05:58 -0300

    test implementation for a describe

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,22 +1,24 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { test, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close
-})
+  afterAll(async () => {
+    await app.close
+  })
 
-test('User can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transactin',
-      amount: 5300,
-      type: 'credit',
-    })
-    .expect(201)
+  test('User can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transactin',
+        amount: 5300,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
